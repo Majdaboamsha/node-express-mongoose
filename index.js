bcd45b3c9a59552f8f42195748b967a389336755
Megd Abo Amsha <Megd.aboamsha@osdatasolutions.de>
2022-05-03 12:05:09 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,43 @@
-const _ = require("underscore");
+const express = require('express');
+const app = express();
 
+app.use(express.json());
 
-const result = _.contains([1,2,3,4,5,6],3);
+const courses = [
+    {id: 1, name: 'Course1'},
+    {id: 2, name: 'Course2'},
+    {id: 3, name: 'Course3'},
+    {id: 4, name: 'Course4'}
+]
 
-console.log(result);
+app.get('/', (req, res) => {
+
+    res.send('Welcome to my application');
+
+});
+
+app.get('/api/courses', (req, res) => {
+
+    res.send([1,2,3]);
+
+})
+
+app.get('/api/courses/:id', (req, res) => {
+
+    const course = courses.find(c => c.id === parseInt(req.params.id));
+    if(!course) return res.status(404);
+    else return res.send(course);
+    
+})
+
+app.post('/api/courses/', (req, res)=> {
+    const course = {
+        id: courses.length + 1,
+        name : req.body.name
+    }
+    courses.push(course);
+    res.send(course);
+
+const port = process.env.PORT || 3000;
+console.log(port);
+app.listen(port), () => console.log(`listening on ${port}`)})
